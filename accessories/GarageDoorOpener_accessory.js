24ea498f480c79b15eafcd7424912d0fe88c0686
lboue <lboue@users.noreply.github.com>
2018-06-29 00:08:51 +0200

    Testing ObstructionDetected Characteristic

--- accessories/GarageDoorOpener_accessory.js
+++ accessories/GarageDoorOpener_accessory.js
@@ -7,22 +7,22 @@ var uuid = require('../').uuid;
 var FAKE_GARAGE = {
   opened: false,
   open: function() {
-    console.log("Opening the Garage!");
+    console.log("[GDC] Opening the Garage!");
     //add your code here which allows the garage to open
     FAKE_GARAGE.opened = true;
   },
   close: function() {
-    console.log("Closing the Garage!");
+    console.log("[GDC] Closing the Garage!");
     //add your code here which allows the garage to close
     FAKE_GARAGE.opened = false;
   },
   identify: function() {
     //add your code here which allows the garage to be identified
-    console.log("Identify the Garage");
+    console.log("[GDC] Identify the Garage");
   },
   status: function(){
     //use this section to get sensor values. set the boolean FAKE_GARAGE.opened with a sensor value.
-    console.log("Sensor queried!");
+    console.log("[GDC] Sensor queried!");
     //FAKE_GARAGE.opened = true/false;
   }
 };
@@ -38,13 +38,24 @@ garage
   .getService(Service.AccessoryInformation)
   .setCharacteristic(Characteristic.Manufacturer, "Liftmaster")
   .setCharacteristic(Characteristic.Model, "Rev-1")
-  .setCharacteristic(Characteristic.SerialNumber, "TW000165");
+  //.setCharacteristic(Characteristic.SerialNumber, "TW000165");
+  .setCharacteristic(Characteristic.SerialNumber, "TW000165")
+  .setCharacteristic(Characteristic.ObstructionDetected, true);
 
 garage.on('identify', function(paired, callback) {
   FAKE_GARAGE.identify();
   callback();
 });
 
+//  set TargetDoorState
+/*
+ * The value property of CurrentDoorState must be one of the following:
+Characteristic.CurrentDoorState.OPEN = 0;
+Characteristic.CurrentDoorState.CLOSED = 1;
+Characteristic.CurrentDoorState.OPENING = 2;
+Characteristic.CurrentDoorState.CLOSING = 3;
+Characteristic.CurrentDoorState.STOPPED = 4;
+ */
 garage
   .addService(Service.GarageDoorOpener, "Garage Door")
   .setCharacteristic(Characteristic.TargetDoorState, Characteristic.TargetDoorState.CLOSED) // force initial state to CLOSED
@@ -64,10 +75,15 @@ garage
       garage
         .getService(Service.GarageDoorOpener)
         .setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.OPEN);
+      
+      // Fake Obstruction
+      garage
+        .getService(Service.GarageDoorOpener)
+        .setCharacteristic(Characteristic.ObstructionDetected, true);
     }
   });
 
-
+//  get CurrentDoorState
 garage
   .getService(Service.GarageDoorOpener)
   .getCharacteristic(Characteristic.CurrentDoorState)
@@ -77,11 +93,32 @@ garage
     FAKE_GARAGE.status();
 
     if (FAKE_GARAGE.opened) {
-      console.log("Query: Is Garage Open? Yes.");
+      console.log("[GDC] Query: Is Garage Open? Yes.");
       callback(err, Characteristic.CurrentDoorState.OPEN);
     }
     else {
-      console.log("Query: Is Garage Open? No.");
+      console.log("[GDC] Query: Is Garage Open? No.");
       callback(err, Characteristic.CurrentDoorState.CLOSED);
     }
+  });
+
+//  get ObstructionDetected
+garage
+  .getService(Service.GarageDoorOpener)
+  .getCharacteristic(Characteristic.ObstructionDetected)
+  .on('get', function(callback) {
+
+    var err = null;
+    FAKE_GARAGE.status();
+
+    
+        if (FAKE_GARAGE.opened) {
+      console.log("[GDC] Query: Is Garage obstructed? Yes.");
+      callback(err, Characteristic.ObstructionDetected.OPEN);
+    }
+    else {
+      console.log("[GDC] Query: Is Garage obsctructed? No.");
+      callback(err, Characteristic.ObstructionDetected.CLOSED);
+    }
+
   });
